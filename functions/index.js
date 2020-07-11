a95973ca99e08d1bd47ea32163d9f6084ee435c8
Yunior Chavez <yunior.chavez@gmail.com>
2020-07-11 11:32:43 -0800

    partial fix pagination on profiles Partial fix pagination on profiles
    , fix permission to retrive profiles

--- functions/index.js
+++ functions/index.js
@@ -30,29 +30,29 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(async (req, res, next) => {
-  try {
-    const userLogin = await auth.verifyIdToken(req.idToken);
-    req.userLogin = await getProfile(userLogin.uid);
-    return next();
-  } catch (e) {
-    return next(e);
-  }
-});
-
 // app.use(async (req, res, next) => {
 //   try {
-//     //const id = '6KkcyToAmdQnmpdr7HTxIFYuZEI2'; // admin id
-//     // const id = 'NSs59e3B3nhEmeqWGYqJdbLVpBD3'; // clinic id
-//     const id = 'qQqcCclJu6NVdFdDoRyhSfj6cqf1'; // doctor id
-//     //const id = 'VYxiXk9ZKldZTaJYd7KpjvWZDjp1'; // enfermera id
-//     //const id = '8nFFoW1hILdsCRq0zgDUoHQyVXs1'; // paciente id
-//     req.userLogin = await getProfile(id);
+//     const userLogin = await auth.verifyIdToken(req.idToken);
+//     req.userLogin = await getProfile(userLogin.uid);
 //     return next();
 //   } catch (e) {
 //     return next(e);
 //   }
 // });
+
+app.use(async (req, res, next) => {
+  try {
+    //const id = '6KkcyToAmdQnmpdr7HTxIFYuZEI2'; // admin id
+    // const id = 'NSs59e3B3nhEmeqWGYqJdbLVpBD3'; // clinic id
+    const id = 'qQqcCclJu6NVdFdDoRyhSfj6cqf1'; // doctor id
+    //const id = 'VYxiXk9ZKldZTaJYd7KpjvWZDjp1'; // enfermera id
+    //const id = '8nFFoW1hILdsCRq0zgDUoHQyVXs1'; // paciente id
+    req.userLogin = await getProfile(id);
+    return next();
+  } catch (e) {
+    return next(e);
+  }
+});
 
 app.use("/", require("./routes/index"));
 
